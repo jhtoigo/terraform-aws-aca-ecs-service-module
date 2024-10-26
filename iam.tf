fa90bacbdd46ff878cf1beeb90b5125b7d9b8dac
Joao Henrique Toigo <jhtoigo@acad.ftec.com.br>
2024-10-26 13:57:40 -0300

    fix: iam role

--- iam.tf
+++ iam.tf
@@ -1,20 +1,20 @@
-resource "aws_iam_role" "service_excution_role" {
+resource "aws_iam_role" "service_execution_role" {
   name = format("%s-%s-service-role", var.cluster_name, var.service_name)
+
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
         Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Sid    = ""
         Principal = {
-          Service = "ecs.amazonaws.com"
+          Service = "ecs-tasks.amazonaws.com"
         }
         Effect = "Allow"
-        Sid    = "*"
+        Sid    = ""
       },
     ]
   })
+
 }
 
 resource "aws_iam_role_policy" "service_execution_role" {
